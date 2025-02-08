48d9af4018bbfb3884d4dfaa4e7595af880a41d6
ArsalGitHub <arsal3414@gmail.com>
2025-02-08 09:01:05 +0500

    fix the tailwind csss for the products listing in popular product section including its responsiveness

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,29 +1,23 @@
 
 
-export const ProductCard = ({productDetails}) => {
+export const ProductCard = ({ productDetails }) => {
 
-  return (
-    <>
-        <section id="popular-products" className="pt-20">
-            <div className="container mx-auto px-4">
-                <h2 className="text-2xl font-bold mb-10">Popular products</h2>
-                <div className="flex flex-wrap -mx-4">
-                
-                    <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
-                    <div className="bg-white p-3 rounded-lg shadow-lg">
-                        <img src={productDetails.imageUrl} alt="Product 1" className="w-full object-cover mb-4 rounded-lg" />
-                        <a href="#" className="text-lg font-semibold mb-2">{productDetails.name}</a>
-                        <p className="my-2">Women</p>
-                        <div className="flex items-center mb-4">
+    return (
+        <>
+
+            <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
+                <div className="bg-white p-3 rounded-lg shadow-lg">
+                    <img src={productDetails.imageUrl} alt="Product 1" className="w-full object-cover mb-4 rounded-lg" />
+                    <a href="#" className="text-lg font-semibold mb-2">{productDetails.name}</a>
+                    <p className="my-2">Women</p>
+                    <div className="flex items-center mb-4">
                         <span className="text-lg font-bold text-primary">{productDetails.price}</span>
                         <span className="text-sm line-through ml-2">$24.99</span>
-                        </div>
-                        <button className="bg-red-500 border border-transparent hover:bg-transparent hover:border-red-500 hover:text-red-500 font-semibold py-2 px-4 rounded-full w-full text-white hover:cursor-pointer">Add to Cart</button>
                     </div>
-                    </div>
+                    <button className="bg-red-500 border border-transparent hover:bg-transparent hover:border-red-500 hover:text-red-500 font-semibold py-2 px-4 rounded-full w-full text-white hover:cursor-pointer">Add to Cart</button>
                 </div>
             </div>
-        </section>
-    </>
-  )
+
+        </>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { ProductCard } from "../components/ProductCard";
 import { useEffect } from "react";
 import { setProducts } from "../redux/productSlice";
-import {homeProduct} from "../assets/sampleData";
+import { homeProduct } from "../assets/sampleData";
 
 
 const Home = () => {
@@ -15,44 +15,49 @@ const Home = () => {
   const dispatch = useDispatch();
   const productData = useSelector(state => state.product)
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(setProducts(homeProduct))
   }, [])
 
 
-    return (
-      <>
-        <section className="flex flex-col md:flex-row items-center justify-between p-6 md:p-12 bg-gray-100 mt-16">
-          {/* Categories */}
-          <div className="w-full md:w-1/4 space-y-4 pt-10">
-            <h2 className="text-xl font-bold">Shop by Categories</h2>
-            <ul className="space-y-2">
-                {Categories.map((category, index) => (
-                    <li key={index} className="p-2 bg-white shadow rounded-lg cursor-pointer hover:bg-gray-200">{category}</li>
-                ))}
+  return (
+    <>
+      <section className="flex flex-col md:flex-row items-center justify-between p-6 md:p-12 bg-gray-100 mt-16">
+        {/* Categories */}
+        <div className="w-full md:w-1/4 space-y-4 pt-10">
+          <h2 className="text-xl font-bold">Shop by Categories</h2>
+          <ul className="space-y-2">
+            {Categories.map((category, index) => (
+              <li key={index} className="p-2 bg-white shadow rounded-lg cursor-pointer hover:bg-gray-200">{category}</li>
+            ))}
 
-            </ul>
+          </ul>
+        </div>
+
+        {/* Hero Image */}
+        <div className="w-full md:w-3/4 flex justify-center mt-6 md:mt-0 pt-10">
+          <img
+            src={Carousel}
+            alt="Hero"
+            className="rounded-lg shadow-lg w-full md:w-3/4"
+          />
+        </div>
+      </section>
+      <InfoSection />
+      <CategorySection />
+
+      <section id="popular-products" className="pt-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-bold mb-10">Popular products</h2>
+          <div className="flex flex-wrap -mx-4">
+            {productData.products.map((elem, index) => (
+              <ProductCard key={index} productDetails={elem} />
+            ))}
           </div>
-          
-          {/* Hero Image */}
-          <div className="w-full md:w-3/4 flex justify-center mt-6 md:mt-0 pt-10">
-            <img
-              src={Carousel}
-              alt="Hero"
-              className="rounded-lg shadow-lg w-full md:w-3/4"
-            />
-          </div>
-        </section>
-        <InfoSection />
-        <CategorySection />
-
-          { productData.products.map((elem, index)=>(
-
-            <ProductCard key={index} productDetails={elem} />
-
-          )) }
-        </>
-      );
-  };
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home
