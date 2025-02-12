3ed11cfcfcb8add457a918abad896fabd1a0bed0
ArsalGitHub <arsal3414@gmail.com>
2025-02-12 23:17:13 +0500

    adjust the icon in card info section in the center of the card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -3,7 +3,7 @@ const ServiceCard = ({ icon, title, details }) => {
   return (
     <>
       <div className="rounded-2xl bg-white p-6 shadow-md transition-all duration-300 hover:shadow-xl dark:bg-gray-800">
-        <div className="mb-6 flex h-16 w-16 items-center justify-center rounded-xl bg-primary text-red-500 text-3xl">
+        <div className="mb-6 flex items-center justify-center rounded-xl bg-primary text-red-500 text-3xl">
           {icon}
         </div>
         <h4 className="mb-3 text-xl font-semibold text-gray-900 dark:text-white">
