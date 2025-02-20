332407fe001c1d75242d063a11dfcde47e318d92
ArsalGitHub <arsal3414@gmail.com>
2025-02-20 15:46:09 +0500

    make the functionality to totally remove or delete items from the cart on the cart list by clikng on the button

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,112 +3,118 @@ import { TbGardenCartOff } from "react-icons/tb";
 import { useState } from "react";
 import Modal from "../components/Modal";
 import ChangeAddress from "../components/ChangeAddress";
+import { FaTrashCan } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../redux/cartSlice";
 
 const Cart = () => {
   const productsInCart = useSelector(state => state.cart)
   const [isOpen, setIsOpen] = useState(false);
-  const [address , changeAddress] = useState('Jelflon street, 0005, Flat 8')
-
+  const [address, changeAddress] = useState('Jelflon street, 0005, Flat 8')
+  const dispatch = useDispatch()
 
   return (
     <>
-    { (productsInCart.productsInCart.length > 0) ? 
-    <div className="bg-gray-100 py-8 mt-24">
-      <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-semibold mb-4 text-red-500">Shopping Cart</h1>
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="md:w-3/4">
-            <div className="bg-white rounded-lg shadow-md p-6 mb-4">
-              <table className="w-full">
-                <thead>
-                  <tr>
-                    <th className="text-left font-semibold">Product</th>
-                    <th className="text-left font-semibold">Price</th>
-                    <th className="text-left font-semibold">Quantity</th>
-                    <th className="text-left font-semibold">Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {productsInCart.productsInCart.map((product, index)=> (                    
-                  <tr key={index}>
-                    <td className="py-4">
-                      <div className="flex items-center">
-                        <img
-                          className="h-16 w-16 mr-4"
-                          src={'.'+product.image}
-                          alt="Product"
-                        />
-                        <span className="font-semibold">{product.name}</span>
-                      </div>
-                    </td>
-                    <td className="py-4">${product.individualItemPrice}</td>
-                    <td className="py-4">
-                      <div className="flex items-center">
-                        <button className="border rounded-md py-2 px-4 mr-2">-</button>
-                        <span className="text-center w-8">{product.quantity}</span>
-                        <button className="border rounded-md py-2 px-4 ml-2">+</button>
-                      </div>
-                    </td>
-                    <td className="py-4">${product.price}</td>
-                  </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="md:w-1/4">
-            <div className="bg-white rounded-lg shadow-md p-6">
-              <h2 className="text-lg font-semibold mb-4">Summary</h2>
-              {/* <div className="flex justify-between mb-2">
+      {(productsInCart.productsInCart.length > 0) ?
+        <div className="bg-gray-100 py-8 mt-24">
+          <div className="container mx-auto px-4">
+            <h1 className="text-2xl font-semibold mb-4 text-red-500">Shopping Cart</h1>
+            <div className="flex flex-col md:flex-row gap-4">
+              <div className="md:w-3/4">
+                <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+                  <table className="w-full">
+                    <thead>
+                      <tr>
+                        <th className="text-left font-semibold">Product</th>
+                        <th className="text-left font-semibold">Price</th>
+                        <th className="text-left font-semibold">Quantity</th>
+                        <th className="text-left font-semibold">Total</th>
+                        <th className="text-left font-semibold">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {productsInCart.productsInCart.map((product, index) => (
+                        <tr key={index}>
+                          <td className="py-4">
+                            <div className="flex items-center">
+                              <img
+                                className="h-16 w-16 mr-4"
+                                src={'.' + product.image}
+                                alt="Product"
+                              />
+                              <span className="font-semibold">{product.name}</span>
+                            </div>
+                          </td>
+                          <td className="py-4">${product.individualItemPrice}</td>
+                          <td className="py-4">
+                            <div className="flex items-center">
+                              <button className="border rounded-md py-2 px-4 mr-2">-</button>
+                              <span className="text-center w-8">{product.quantity}</span>
+                              <button className="border rounded-md py-2 px-4 ml-2">+</button>
+                            </div>
+                          </td>
+                          <td className="py-4">${product.price}</td>
+                          <td className="py-4"><FaTrashCan className="mx-auto text-red-500 hover:cursor-pointer" onClick={ ()=> dispatch(removeFromCart(product.id)) } /></td>
+
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div className="md:w-1/4">
+                <div className="bg-white rounded-lg shadow-md p-6">
+                  <h2 className="text-lg font-semibold mb-4">Summary</h2>
+                  {/* <div className="flex justify-between mb-2">
                 <span>Subtotal</span>
                 <span>$19.99</span>
               </div> */}
-              <hr className="my-2" />
-              <div className="flex justify-between mb-2">
-                <span className="font-semibold">Total Items</span>
-                <span className="font-semibold">{productsInCart.totalQuantity}</span>
+                  <hr className="my-2" />
+                  <div className="flex justify-between mb-2">
+                    <span className="font-semibold">Total Items</span>
+                    <span className="font-semibold">{productsInCart.totalQuantity}</span>
+                  </div>
+                  <br />
+                  <div className="flex justify-between mb-2">
+                    <span>Address:</span>
+                    <span>{address}</span>
+                  </div>
+                  <br />
+                  <div className="flex justify-between mb-2">
+                    {/* <a href="">Change Address</a> */}
+                    {/* Open Modal Button */}
+                    <button
+                      onClick={() => setIsOpen(true)}
+                      className="bg-red-500 text-white px-4 py-2 rounded-md hover:cursor-pointer"
+                    >
+                      Change Address
+                    </button>
+                  </div>
+
+                  <hr className="my-2" />
+                  <div className="flex justify-between mb-2">
+                    <span className="font-semibold">Total</span>
+                    <span className="font-semibold">${productsInCart.totalPrice}</span>
+                  </div>
+                  <button className="bg-red-500 text-white py-2 px-4 rounded-lg mt-4 w-full">
+                    Checkout
+                  </button>
+                </div>
               </div>
-              <br />
-              <div className="flex justify-between mb-2">
-                <span>Address:</span>
-                <span>{address}</span>
-              </div>
-              <br />
-              <div className="flex justify-between mb-2">
-                {/* <a href="">Change Address</a> */}
-                {/* Open Modal Button */}
-                <button
-                  onClick={() => setIsOpen(true)}
-                  className="bg-red-500 text-white px-4 py-2 rounded-md hover:cursor-pointer"
-                >
-                  Change Address
-                </button>
-              </div>
-                    
-              <hr className="my-2" />
-              <div className="flex justify-between mb-2">
-                <span className="font-semibold">Total</span>
-                <span className="font-semibold">${productsInCart.totalPrice}</span>
-              </div>
-              <button className="bg-red-500 text-white py-2 px-4 rounded-lg mt-4 w-full">
-                Checkout
-              </button>
             </div>
           </div>
+          <Modal isOpen={isOpen} setIsOpen={setIsOpen} ChangeAddress={<ChangeAddress changeAddress={changeAddress} setIsOpen={setIsOpen} />} />
         </div>
-      </div>
-        <Modal isOpen={isOpen} setIsOpen={setIsOpen} ChangeAddress={<ChangeAddress changeAddress={changeAddress} setIsOpen={setIsOpen} />} />
-    </div>
 
 
-    : <div> <TbGardenCartOff className="block mx-auto w-48 h-48 mt-42 text-red-500" /> 
-            <h3 className="block text-center text-2xl text-gray-500 py-4">( The cart is empty.)</h3>
+        : <div> <TbGardenCartOff className="block mx-auto w-48 h-48 mt-42 text-red-500" />
+          <h3 className="block text-center text-2xl text-gray-500 py-4">( The cart is empty.)</h3>
 
-    </div> }
+        </div>}
 
     </>
-        
-      );
+
+  );
 }
 
 export default Cart
--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -1,44 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    productsInCart : [],
-    totalPrice : 0,
-    totalQuantity : 0
+    productsInCart: [],
+    totalPrice: 0,
+    totalQuantity: 0
 }
 
 
 export const cartSlice = createSlice({
-    name : 'cart',
+    name: 'cart',
     initialState,
-    reducers : {
-        addTocart : (state, action) =>{
+    reducers: {
+        addTocart: (state, action) => {
             const newItem = action.payload
             console.log(newItem)
-            const itemObjectExist = state.productsInCart.find( (elem)=> elem.id == newItem.id )
+            const itemObjectExist = state.productsInCart.find((elem) => elem.id == newItem.id)
 
-            if(itemObjectExist){
+            if (itemObjectExist) {
                 itemObjectExist.quantity++
                 itemObjectExist.price = parseFloat(itemObjectExist.price) + parseFloat(newItem.price)
-            }else{
+            } else {
                 state.productsInCart.push({
-                    id : newItem.id,
-                    name : newItem.name,
-                    price : newItem.price,
-                    quantity : 1,
-                    individualItemPrice : newItem.price,
-                    image : newItem.imageUrl
+                    id: newItem.id,
+                    name: newItem.name,
+                    price: newItem.price,
+                    quantity: 1,
+                    individualItemPrice: newItem.price,
+                    image: newItem.imageUrl
                 })
             }
 
             state.totalPrice += parseFloat(newItem.price);
             state.totalQuantity++;
+        },
 
+        removeFromCart: (state, action) => {
+            const removingId = action.payload
+            const itemObjectExist = state.productsInCart.find((elem) => elem.id == removingId)
+
+            if(itemObjectExist){
+                state.totalPrice = state.totalPrice - itemObjectExist.price
+                state.totalQuantity = state.totalQuantity - itemObjectExist.quantity
+                state.productsInCart = state.productsInCart.filter((elem)=> elem.id != removingId)
+            }
         }
     }
 
 })
 
 
-export const { addTocart } = cartSlice.actions;
+export const { addTocart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
