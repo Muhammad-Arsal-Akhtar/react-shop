972ec6b05805ad2c47a08466876cad6740fbf2cd
ArsalGitHub <arsal3414@gmail.com>
2025-02-21 20:29:16 +0500

    set the section for the information in checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -6,7 +6,7 @@ const Checkout = () => {
   const productsInCart = useSelector(state => state.cart)
   const dispatch = useDispatch()
 
-  const [openAccordion, setOpenAccordion] = useState(null);
+  const [openAccordion, setOpenAccordion] = useState(1);
 
   const toggleAccordion = (id) => {
     setOpenAccordion(openAccordion === id ? null : id);
@@ -30,7 +30,7 @@ const Checkout = () => {
                       aria-expanded={openAccordion === 1}
                       aria-controls="accordion-collapse-body-1"
                     >
-                      <span>What is Flowbite?</span>
+                      <span>Billing Information</span>
                       <svg
                         data-accordion-icon
                         className={`w-3 h-3 shrink-0 transform ${openAccordion === 1 ? "rotate-180" : ""}`}
@@ -55,9 +55,24 @@ const Checkout = () => {
                     aria-labelledby="accordion-collapse-heading-1"
                   >
                     <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                      <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        Flowbite is an open-source library of interactive components built on top of Tailwind CSS.
-                      </p>
+                      <form>
+                        <div className="grid gap-6 mb-6 md:grid-cols-1">
+                          <div>
+                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter name" required />
+                          </div>
+                          <div>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Email" required />
+                          </div>
+
+                          <div>
+                            <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
+                            <input type="number" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Phone number" required />
+                          </div>
+
+                        </div>
+                      </form>
                     </div>
                   </div>
 
@@ -70,7 +85,7 @@ const Checkout = () => {
                       aria-expanded={openAccordion === 2}
                       aria-controls="accordion-collapse-body-2"
                     >
-                      <span>Is there a Figma file available?</span>
+                      <span>Shipping Information</span>
                       <svg
                         data-accordion-icon
                         className={`w-3 h-3 shrink-0 transform ${openAccordion === 2 ? "rotate-180" : ""}`}
@@ -95,9 +110,95 @@ const Checkout = () => {
                     aria-labelledby="accordion-collapse-heading-2"
                   >
                     <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-                      <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        Flowbite is first conceptualized and designed using the Figma software.
-                      </p>
+                      <form>
+                        <div className="grid gap-6 mb-6 md:grid-cols-1">
+                          <div>
+                            <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                            <input type="text" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="123 Main St" required />
+                          </div>
+                          <div>
+                            <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
+                            <input type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="New York" required />
+                          </div>
+                          <div>
+                            <label htmlFor="zip_code" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zip Code</label>
+                            <input type="text" id="zip_code" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="10001" required />
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+
+                  {/* Accordion Item 3 */}
+                  <h2 id="accordion-collapse-heading-3">
+                    <button
+                      type="button"
+                      className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                      onClick={() => toggleAccordion(3)}
+                      aria-expanded={openAccordion === 3}
+                      aria-controls="accordion-collapse-body-3"
+                    >
+                      <span>Payment Method</span>
+                      <svg
+                        data-accordion-icon
+                        className={`w-3 h-3 shrink-0 transform ${openAccordion === 3 ? "rotate-180" : ""}`}
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9 5 5 1 1 5"
+                        />
+                      </svg>
+                    </button>
+                  </h2>
+                  <div
+                    id="accordion-collapse-body-2"
+                    className={openAccordion === 3 ? "block" : "hidden"}
+                    aria-labelledby="accordion-collapse-heading-2"
+                  >
+                    <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                      <form>
+                        <div className="grid gap-6 mb-6 md:grid-cols-1">
+                          <div>
+                            <div className="flex items-center mb-4">
+                              <input
+                                id="default-radio-1"
+                                type="radio"
+                                name="default-radio"
+                                value="default-radio-1"
+                                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                              />
+                              <label
+                                htmlFor="default-radio-1"
+                                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                              >
+                                Cash on Delivery
+                              </label>
+                            </div>
+
+                            <div className="flex items-center">
+                              <input
+                                id="default-radio-2"
+                                type="radio"
+                                name="default-radio"
+                                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                              />
+                              <label
+                                htmlFor="default-radio-2"
+                                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                              >
+                                Debit Card
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
                     </div>
                   </div>
                 </div>
