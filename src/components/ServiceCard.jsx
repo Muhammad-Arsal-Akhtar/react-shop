6cec962be2fc778d989ed2dd603199cb5a0c59f9
ArsalGitHub <arsal3414@gmail.com>
2025-03-14 23:15:53 +0500

    make the text center of fot the info card section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@
 const ServiceCard = ({ icon, title, details }) => {
   return (
     <>
-      <div className="rounded-2xl bg-white p-6 shadow-md transition-all duration-300 hover:shadow-xl dark:bg-gray-800">
+      <div className="rounded-2xl bg-white p-6 shadow-md transition-all duration-300 hover:shadow-xl dark:bg-gray-800 text-center">
         <div className="mb-6 flex items-center justify-center rounded-xl bg-primary text-red-500 text-3xl">
           {icon}
         </div>
